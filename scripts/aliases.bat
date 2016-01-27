25e2d5b1a13e542672d7ba622237ffbd117ea93d
OlivierParent <olivier.parent@arteveldehs.be>
2016-01-27 15:05:21 +0100

    [FIX]

---
+++ scripts/aliases.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+doskey c=CD %USERPROFILE%\Code\
+doskey h=CD %USERPROFILE%
+
+doskey hosts=code %Systemroot%\System32\Drivers\Etc\hosts
+
+doskey cmp=CD %USERPROFILE%\Code\cmp.local\
+doskey cmp1=CD %USERPROFILE%\Code\cmp1.local\
+doskey cmp2=CD %USERPROFILE%\Code\cmp2.local\
+doskey cmp3=CD %USERPROFILE%\Code\cmp3.local\
+doskey ed=CD %USERPROFILE%\Code\ed.local\
+doskey nmdad2=CD %USERPROFILE%\Code\nmdad2.local\
+doskey nmdad3=CD %USERPROFILE%\Code\nmdad3.local\
+doskey mmtoep=CD %USERPROFILE%\Code\mmtoep.local\
