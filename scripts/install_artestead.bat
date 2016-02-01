1a83b1926761aa6387cf145a272a6d440a08e109
olivierparent <olivier.parent@arteveldehs.be>
2016-02-01 09:04:27 +0100

    [WIP]

--- scripts/install_artestead.bat
+++ scripts/install_artestead.bat
@@ -1,4 +1,2 @@
 @ECHO OFF
-vagrant plugin install vagrant-hostsupdater
-composer config --global repositories.artestead vcs https://github.com/gdmgent/artestead.git
-composer g require hirak/prestissimo gdmgent/artestead
+vagrant plugin install vagrant-hostsupdater && composer config --global repositories.artestead vcs https://github.com/gdmgent/artestead.git && composer g require hirak/prestissimo gdmgent/artestead
