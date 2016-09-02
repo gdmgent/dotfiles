4f6b6f2387c025725e8d5a5352508b474ec4d988
OlivierParent <olivier.parent@parteveldehs.be>
2016-09-02 13:41:01 +0200

    [WIP]

--- dotfiles.psm1
+++ dotfiles.psm1
@@ -128,8 +128,17 @@ function InstallComposer {
         sh -c 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer'
         Write-Host 'Installed version of Composer: ' -NoNewline
         composer --version
-    } else {
-        Write-Warning -Message 'This is for macOS only.'
+    } elseif ($IsWindows) {
+        Write-Host 'Downloading Composer installer...'
+        $Urn = 'Composer-Setup.exe'
+        $Uri = "https://getcomposer.org/$Urn"
+        $InstallerFile = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $InstallerFile
+        if (Test-Path $InstallerFile) {
+            Write-Host 'Running Composer Installer...'
+            Invoke-Expression $InstallerFile
+            Remove-Item $InstallerFile
+        }
     }
 }
 
@@ -156,7 +165,17 @@ function InstallGit {
         Write-Host 'Installed version of Git: ' -NoNewline
         git --version
     } elseif ($IsWindows) {
-        cmd.exe /C start https://git-scm.com/download/win
+        Write-Host 'Downloading Git installer...'
+        $Version = 'v2.9.2.windows.1'
+        $Urn = 'Git-2.9.2-64-bit.exe'
+        $Uri = "https://github.com/git-for-windows/git/releases/download/$Version/$Urn"
+        $InstallerFile = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $InstallerFile
+        if (Test-Path $InstallerFile) {
+            Write-Host 'Running Git installer...'
+            Invoke-Expression $InstallerFile
+            Remove-Item $InstallerFile
+        }
     }
 }
 
@@ -179,7 +198,22 @@ function InstallNvm {
         Write-Host 'Installed version of NVM: ' -NoNewline
         nvm --version
     } elseif ($IsWindows) {
-        
+        Write-Host 'Downloading Node Version Manager...'
+        $Version = '1.1.1'
+        $Urn = 'nvm-setup.zip'
+        $Uri = "https://github.com/coreybutler/nvm-windows/releases/download/$Version/$Urn"
+        $InstallerArchive = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $InstallerArchive
+        if (Test-Path $InstallerArchive) {
+            Write-Host 'Running Node Version Manager installer...'
+            Expand-Archive -Path $InstallerArchive -DestinationPath $env:TEMP -Force
+            $InstallerFile = Join-Path -Path $env:TEMP -ChildPath 'nvm-setup.exe'
+            if (Test-Path $InstallerFile) {
+                Remove-Item -Path $InstallerArchive
+                Invoke-Expression $InstallerFile
+                Remove-Item $InstallerFile
+            }
+        }
     }
 }
 
@@ -200,33 +234,104 @@ function InstallPhp {
     if ($IsOSX) {
         Write-Host 'Using Homebrew to install PHP 7.0...'
         sh -c 'brew tap homebrew/php && brew install php70 php70-mcrypt'
+    } elseif ($IsWindows) {
+        Write-Host 'Downloading PHP 7.0...'
+        $Version = '7.0.10'
+        $Urn = "php-$Version-nts-Win32-VC14-x64.zip"
+        $Uri = "http://windows.php.net/downloads/releases/$Urn"
+        $OutFile = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $OutFile
+        if (Test-Path $OutFile) {
+            $DestinationPath = 'C:\php'
+            if ((Test-Path $DestinationPath) -and !(Test-Path "$DestinationPath.bak")) {
+                Write-Host "Making a backup of previously installed version..."
+                Move-Item -Path $DestinationPath -Destination "$DestinationPath.bak"
+            }
+            Write-Host 'Installing PHP 7.0...'
+            Expand-Archive -Path $OutFile -DestinationPath $DestinationPath -Force
+            Remove-Item -Path $OutFile
+            Write-Host 'Configuring PHP 7.0...'
+            $ConfigFile = Get-Content C:\php\php.ini-development
+            $Replacements = @(
+                'extension=php_curl.dll'
+                'extension=php_mbstring.dll'
+                'extension=php_openssl.dll'
+            )
+            foreach ($Replacement in $Replacements) {
+                $ConfigFile = $ConfigFile.Replace(";$Replacement", $Replacement)
+            }
+            Set-Content -Path (Join-Path -Path $DestinationPath -ChildPath 'php.ini') -Value $ConfigFile
+        }
+    }
+    if (Get-Command php -errorAction SilentlyContinue) {
         Write-Host 'Installed version of PHP: ' -NoNewline
         php -v
-    } else {
-        Write-Warning -Message 'This is for macOS only.'
     }
 }
 
 function InstallRuby {
-    Write-Host 'Using Homebrew to install Ruby...'
-    sh -c 'brew install ruby'
-    Write-Host 'Installed version of Ruby: ' -NoNewline
-    ruby --version
-    Write-Host 'Installed version of Gem: ' -NoNewline
-    gem --version
+    if ($IsOSX) {
+        Write-Host 'Using Homebrew to install Ruby...'
+        sh -c 'brew install ruby'
+    } elseif ($IsWindows) {
+        Write-Host 'Downloading Ruby installer...'
+        $Version = '2.3.1'
+        $Urn = "rubyinstaller-$Version-x64.exe"
+        $Uri = "http://dl.bintray.com/oneclick/rubyinstaller/$Urn"
+        $InstallerFile = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $InstallerFile
+        if (Test-Path $InstallerFile) {
+            Write-Host 'Running Ruby installer...'
+            Invoke-Expression $InstallerFile
+            Remove-Item $InstallerFile
+        }
+        Write-Host 'Downloading Ruby DevKit installer...'
+        $Version = '4.7.2-20130224-1432'
+        $Urn = "DevKit-mingw64-64-$Version-sfx.exe"
+        $Uri = "http://dl.bintray.com/oneclick/rubyinstaller/$Urn"
+        $InstallerFile = Join-Path -Path $env:TEMP -ChildPath $Urn
+        Invoke-WebRequest -Uri $Uri -OutFile $InstallerFile
+        if (Test-Path $InstallerFile) {
+            Write-Host 'Running Ruby DevKit installer...'
+            Invoke-Expression $InstallerFile
+            Remove-Item $InstallerFile
+        }
+    }
+    if (Get-Command ruby -errorAction SilentlyContinue) {
+        Write-Host 'Installed version of Ruby: ' -NoNewline
+        ruby --version
+        if (Get-Command gem -errorAction SilentlyContinue) {
+            Write-Host 'Installed version of Gem: ' -NoNewline
+            gem --version
+        }
+    } else {
+        Write-Warning -Message 'Ruby is not correctly installed.'
+    }
 }
 
 function RemoveLocalArtestead {
     $file = 'Artestead.yaml'
     if (Test-Path $file) {
         Write-Host 'Removing Local Artestead...'
-        Get-Command artestead
+        if (Test-Path Vagrantfile) {
+            vagrant destroy
+            Remove-Item -Path .vagrant -Recurse
+            Remove-Item -Path Vagrantfile
+        }
+        Remove-Item -Path .gitignore
+        Remove-Item -Path *.sh
+        Remove-Item -Path composer.*
+        Remove-Item -Path vendor -Recurse
+        Remove-Item -Path $file
     } else {
         Write-Warning -Message "This is not an Artestead project. Could not find '$file' in this directory."
     }
 }
 
 function RemoveAndroidStudio {
+    if ($IsOSX) {
+        
+    }
     # rm -Rf /Applications/Android\ Studio.app
     # rm -Rf ~/Library/Preferences/AndroidStudio*
     # rm ~/Library/Preferences/com.google.android.studio.plist
