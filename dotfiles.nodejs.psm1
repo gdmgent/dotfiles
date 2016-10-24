dd5bd077537fa87dc27b29b6b22b7101f7354942
olivierparent <olivier.parent@arteveldehs.be>
2016-10-24 11:35:57 +0200

    [WIP]

--- dotfiles.nodejs.psm1
+++ dotfiles.nodejs.psm1
@@ -1,6 +1,6 @@
 if ($IsOSX) {
     New-Alias -Name node -Value Node -Scope Global
-    Set-Variable -Name NodeJsPath -Value "$HOME/.nvm/versions/node/" -Option Constant -Scope Global
+    Set-Variable -Name NodeJsPath -Value "$HOME/.nvm/versions/node" -Option Constant -Scope Global
 }
 
 # Node Version Manager
