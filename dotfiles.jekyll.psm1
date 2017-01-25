e29580839d93d05354aa73a469ef6a9cc146c24b
OlivierParent <olivier.parent@arteveldehs.be>
2017-01-25 18:31:07 +0100

    [WIP]

--- dotfiles.jekyll.psm1
+++ dotfiles.jekyll.psm1
@@ -1,48 +1,55 @@
-function CodeJekyllServe {
-    if (IsJekyllSite) {
-        code .
-        JekyllServe $args
-    }
-}
-New-Alias -Name cjs -Value CodeJekyllServe
-
-function CodeJekyllServeUnpublished {
-    if (IsJekyllSite) {
-        code .
-        JekyllServeUnpublished $args
-    }
-}
-New-Alias -Name cjsu -Value CodeJekyllServeUnpublished
-
 function JekyllServe {
+    [CmdletBinding(PositionalBinding=$false)]
+    Param(
+        [Switch]
+        [Alias('c')]
+        $Code,
+        
+        [Switch]
+        [Alias('d')]
+        $Drafts,
+        
+        [Switch]
+        [Alias('f')]
+        $Future,
+        
+        [Switch]
+        [Alias('i')]
+        $Incremental,
+
+        [Int]
+        [ValidateRange(4000,4999)]
+        [Alias('p')]
+        $Port = 4000,
+
+        [Switch]
+        [Alias('u')]
+        $Unpublished
+    )
     if (IsJekyllSite) {
         $Directory = (Get-Item -Path .).Name
-        $Uri = "http://127.0.0.1:4000/$Directory/"
-        OpenUri -Uri $Uri.Replace('utl_', '')
-        Invoke-Expression -Command "bundle exec jekyll serve $args"
+        $Uri = "http://127.0.0.1:$Port/$Directory/"
+        # OpenUri -Uri $Uri.Replace('utl_', '')
+        $Command = "bundle exec jekyll serve --port=$Port"
+        if ($Code) {
+            code .
+        }
+        if ($Drafts) {
+            $Command += ' --drafts'
+        }
+        if ($Future) {
+            $Command += ' --future'
+        }
+        if ($Incremental) {
+            $Command += ' --incremental'
+        }
+        if ($Unpublished) {
+            $Command += ' --unpublished'
+        }
+        Invoke-Expression -Command $Command
     }
 }
 New-Alias -Name js -Value JekyllServe
-
-function JekyllServeDrafts {
-    JekyllServe --drafts $args
-}
-New-Alias -Name jsd -Value JekyllServeDrafts
-
-function JekyllServeFuture {
-    JekyllServe --future $args
-}
-New-Alias -Name jsf -Value JekyllServeFuture
-
-function JekyllServeIncremental {
-    JekyllServe --incremental $args
-}
-New-Alias -Name jsi -Value JekyllServeIncremental
-
-function JekyllServeUnpublished {
-    JekyllServe --unpublished $args
-}
-New-Alias -Name jsu -Value JekyllServeUnpublished
 
 function IsJekyllSite {
     $File = "_config.yml"
