f4d6861fa9cf6ff56fbf34da023212f50475207a
olivierparent <olivier.parent@arteveldehs.be>
2017-03-05 11:09:26 +0100

    [WIP]

--- dotfiles.core.psm1
+++ dotfiles.core.psm1
@@ -620,7 +620,7 @@ function RemoveLocalArtestead {
     $File = 'Artestead.yaml'
     if (Test-Path -Path $File) {
         Write-Host 'Removing Local Artestead...'
-        $ToRemove = @($File)
+        $ToRemove = @($File, $File+'.example')
         if (Test-Path -Path Vagrantfile) {
             vagrant destroy
             $ToRemove += @('.vagrant', 'Vagrantfile')
@@ -729,139 +729,6 @@ function UpdateComposer {
 function SearchDotfilesCommands {
     Get-Command "$args" | Where-Object { $_.Source -eq 'dotfiles' }
     Get-Alias   "$args" | Where-Object { $_.Source -eq 'dotfiles' -or $_.Source -like 'aliases*' }
-}
-
-function CloneProject {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String]
-        $Name,
-        [String]
-        $DestinationName,
-        [ValidateSet('bitbucket.org','github.com','gitlab.com')]
-        [String]
-        $Service = 'github.com',
-        [String]
-        $Account = 'gdmgent'
-    )
-    $DestinationName = $DestinationName.ToLower()
-    SetLocationPathCode
-    git clone https://$Service/$Account/$Name $DestinationName
-    if ($DestinationName) {
-        SetLocationPathCode $DestinationName
-    } else {
-        SetLocationPathCode $Name
-    }
-}
-
-function CloneSyllabus {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String]
-        $Name,
-        [String]
-        $DestinationName,
-        [ValidateSet('github.com','gitlab.com')]
-        [String]
-        $Service = 'github.com',
-        [String]
-        $Account = 'gdmgent',
-        [Switch]
-        $Master
-    )
-    $Branch = if ($Master) { 'master' } else { 'gh-pages' }
-    $DestinationName = $DestinationName.ToLower()
-    SetLocationPathSyllabi
-    git clone https://$Service/$Account/$Name --branch $Branch --single-branch $DestinationName
-    if ($DestinationName) {
-        SetLocationPathSyllabi $DestinationName
-    } else {
-        SetLocationPathSyllabi $Name
-    }
-}
-
-function NewSyllabus {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [String]
-        $Name,
-        [ValidateSet('github.com','gitlab.com')]
-        [String]
-        $Service = 'github.com',
-        [String]
-        $Account = 'gdmgent',
-        [Switch]
-        $Master
-    )
-    $Name = $Name.ToLower()
-    SetLocationPathSyllabi
-    New-Item -Path $Name -ItemType Directory
-    Set-Location -Path $Name
-    git init
-    git checkout -b gh-pages
-    git remote add origin https://$Service/$Account/$Name.git
-    New-Item -Path README.md -ItemType File
-    git add .
-    git commit -m [WIP]
-    git push --set-upstream origin gh-pages
-}
-
-function PullSyllabi {
-    Param(
-        [Switch]
-        $Force
-    )
-    Push-Location
-    SetLocationPathSyllabi
-    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
-    foreach ($Directory in $Directories) {
-        Push-Location $Directory
-        if (Test-Path -Path .git) {
-            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
-            git add . | Write-Host -ForegroundColor DarkGray
-            if ($Force) {
-                git stash | Write-Host -ForegroundColor DarkGray
-                git stash drop | Write-Host -ForegroundColor DarkGray
-                git pull | Write-Host -ForegroundColor DarkGray
-            } else {
-                git pull | Write-Host -ForegroundColor DarkGray
-            }
-        }
-        Pop-Location
-    }
-    Pop-Location
-}
-
-function PushSyllabi {
-    Push-Location
-    SetLocationPathSyllabi
-    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
-    foreach ($Directory in $Directories) {
-        Push-Location $Directory
-        if (Test-Path -Path .git) {
-            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
-            git add . | Write-Host -ForegroundColor DarkGray
-            git commit -a -m [WIP] | Write-Host -ForegroundColor DarkGray
-            git push | Write-Host -ForegroundColor DarkGray
-        }
-        Pop-Location
-    }
-    Pop-Location
-}
-
-function StatusSyllabi {
-    Push-Location
-    SetLocationPathSyllabi
-    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
-    foreach ($Directory in $Directories) {
-        Push-Location $Directory
-        if (Test-Path -Path .git) {
-            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
-            git status | Write-Host -ForegroundColor DarkGray
-        }
-        Pop-Location
-    }
-    Pop-Location
 }
 
 function X {
--- dotfiles.git.psm1
+++ dotfiles.git.psm1
@@ -51,3 +51,139 @@ function GitStatus {
 }
 New-Alias -Name status -Value GitStatus
 New-Alias -Name sts -Value GitStatus
+
+function CloneProject {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]
+        $Name,
+        [String]
+        $DestinationName,
+        [ValidateSet('bitbucket.org','github.com','gitlab.com')]
+        [String]
+        $Service = 'github.com',
+        [String]
+        $Account = 'gdmgent'
+    )
+    if ($Service -eq 'gitlab.com') {
+        $Git = '.git'
+    }
+    $DestinationName = $DestinationName.ToLower()
+    SetLocationPathCode
+    git clone https://$Service/$Account/$Name$Git $DestinationName
+    if ($DestinationName) {
+        SetLocationPathCode $DestinationName
+    } else {
+        SetLocationPathCode $Name
+    }
+}
+
+function CloneSyllabus {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]
+        $Name,
+        [String]
+        $DestinationName,
+        [ValidateSet('github.com','gitlab.com')]
+        [String]
+        $Service = 'github.com',
+        [String]
+        $Account = 'gdmgent',
+        [Switch]
+        $Master
+    )
+    $Branch = if ($Master) { 'master' } else { 'gh-pages' }
+    $DestinationName = $DestinationName.ToLower()
+    SetLocationPathSyllabi
+    git clone https://$Service/$Account/$Name --branch $Branch --single-branch $DestinationName
+    if ($DestinationName) {
+        SetLocationPathSyllabi $DestinationName
+    } else {
+        SetLocationPathSyllabi $Name
+    }
+}
+
+function NewSyllabus {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]
+        $Name,
+        [ValidateSet('github.com','gitlab.com')]
+        [String]
+        $Service = 'github.com',
+        [String]
+        $Account = 'gdmgent',
+        [Switch]
+        $Master
+    )
+    $Name = $Name.ToLower()
+    SetLocationPathSyllabi
+    New-Item -Path $Name -ItemType Directory
+    Set-Location -Path $Name
+    git init
+    git checkout -b gh-pages
+    git remote add origin https://$Service/$Account/$Name.git
+    New-Item -Path README.md -ItemType File
+    git add .
+    git commit -m [WIP]
+    git push --set-upstream origin gh-pages
+}
+
+function PullSyllabi {
+    Param(
+        [Switch]
+        $Force
+    )
+    Push-Location
+    SetLocationPathSyllabi
+    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
+    foreach ($Directory in $Directories) {
+        Push-Location $Directory
+        if (Test-Path -Path .git) {
+            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
+            git add . | Write-Host -ForegroundColor DarkGray
+            if ($Force) {
+                git stash | Write-Host -ForegroundColor DarkGray
+                git stash drop | Write-Host -ForegroundColor DarkGray
+                git pull | Write-Host -ForegroundColor DarkGray
+            } else {
+                git pull | Write-Host -ForegroundColor DarkGray
+            }
+        }
+        Pop-Location
+    }
+    Pop-Location
+}
+
+function PushSyllabi {
+    Push-Location
+    SetLocationPathSyllabi
+    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
+    foreach ($Directory in $Directories) {
+        Push-Location $Directory
+        if (Test-Path -Path .git) {
+            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
+            git add . | Write-Host -ForegroundColor DarkGray
+            git commit -a -m [WIP] | Write-Host -ForegroundColor DarkGray
+            git push | Write-Host -ForegroundColor DarkGray
+        }
+        Pop-Location
+    }
+    Pop-Location
+}
+
+function StatusSyllabi {
+    Push-Location
+    SetLocationPathSyllabi
+    $Directories = Get-ChildItem -Directory -Name | Where-Object { $_ -match '^((\d{4}|utl|mod)_|syllabus)|.github.io$' }
+    foreach ($Directory in $Directories) {
+        Push-Location $Directory
+        if (Test-Path -Path .git) {
+            Write-Host " $Directory " -BackgroundColor Blue -ForegroundColor White
+            git status | Write-Host -ForegroundColor DarkGray
+        }
+        Pop-Location
+    }
+    Pop-Location
+}
