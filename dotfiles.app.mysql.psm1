1968ce78f24eb3395dbc5c71476d7822f244cb39
olivierparent <olivier.parent@arteveldehs.be>
2017-04-07 16:26:29 +0200

    [WIP]

--- dotfiles.app.mysql.psm1
+++ dotfiles.app.mysql.psm1
@@ -1,19 +1,24 @@
 function MySQLStart {
     mysql.server start
 }
+New-Alias -Name myon -Value MySQLStart
 
 function MySQLStop {
     mysql.server stop
 }
+New-Alias -Name myoff -Value MySQLStop
 
 function MySQLStatus {
     mysql.server status
 }
+New-Alias -Name mysts -Value MySQLStatus
 
 function MySQLInitialize {
-    mysqladmin --user=homestead --password=secret
+    mysql --host=127.0.0.1 --user=root --execute="CREATE USER 'homestead'@'localhost' IDENTIFIED BY 'secret';GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'localhost' WITH GRANT OPTION;"
 }
+New-Alias -Name myinit -Value MySQLInitialize
 
 function MySQLLogin {
-    mysql --user=homestead --password=secret
+    mysql --host=127.0.0.1 --user=homestead --password=secret
 }
+New-Alias -Name mydb -Value MySQLLogin
