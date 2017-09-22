0fbbf26981929f7ca2d4def120b0c8cb4e9a18f8
OlivierParent <olivier.parent@arteveldehs.be>
2017-09-22 14:15:31 +0200

    [FIX] homefolder with spaces

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 if ($isWindows) {
     Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 }
-Set-Content -Path (New-Item -Force $PROFILE -Type File) -Value ('. ' + (Join-Path -Path $PWD -ChildPath dotfiles.ps1))
+Set-Content -Path (New-Item -Force $PROFILE -Type File) -Value ('. ' + '"' + (Join-Path -Path $PWD -ChildPath dotfiles.ps1) + '"')
