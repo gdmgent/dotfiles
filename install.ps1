03141729e722dc9fa7fa73248583134b94097399
OlivierParent <olivier.parent@arteveldehs.be>
2018-03-16 14:02:40 +0100

    [WIP]

--- install.ps1
+++ install.ps1
@@ -1,4 +1,1 @@
-if ($isWindows) {
-    Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-}
 Set-Content -Path (New-Item -Force $PROFILE -Type File) -Value ('. ' + '"' + (Join-Path -Path $PWD -ChildPath dotfiles.ps1) + '"')
